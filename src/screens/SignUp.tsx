14181d49eefbdf1901bdab5a30511b3479cdbf23
enzoguis <enzog0315@gmail.com>
2025-04-02 17:06:27 -0300

    adding axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,19 +1,23 @@
+import BackgroundImg from '@assets/background.png'
+import Logo from '@assets/logo.svg'
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
+import { ToastMessage } from '@components/ToastMessage'
 import {
   Center,
   Heading,
   Image,
   ScrollView,
   Text,
+  useToast,
   VStack,
 } from '@gluestack-ui/themed'
-import BackgroundImg from '@assets/background.png'
-import Logo from '@assets/logo.svg'
-import { Input } from '@components/Input'
-import { Button } from '@components/Button'
-import { useNavigation } from '@react-navigation/native'
-import * as yup from 'yup'
-import { useForm, Controller } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useNavigation } from '@react-navigation/native'
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
+import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
 type FormDataProps = {
   name: string
@@ -36,6 +40,8 @@ const signUpSchema = yup.object({
 })
 
 export function SignUp() {
+  const toast = useToast()
+
   const {
     control,
     handleSubmit,
@@ -46,7 +52,29 @@ export function SignUp() {
 
   const navigation = useNavigation()
 
-  function handleSignUp(data: FormDataProps) {}
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post('/users', { name, email, password })
+      console.log(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível criar a conta. Tente novamente mais tarde.'
+
+      toast.show({
+        placement: 'top',
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
+    }
+  }
 
   function handleGoBack() {
     navigation.goBack()
